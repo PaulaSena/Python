ccf56fa62f6b43fc7a4cffcf829bb50ca401ab3b
PaulaSena <senasilvaanapaula@gmail.com>
2019-04-15 10:42:21 -0300

    if else

---
+++ script-python/b.py
@@ -0,0 +1,10 @@
+nome=input('Qual é seu nome? ')
+idade=input('Qual é a sua idade? ')
+peso=input('Qual é a seu peso? ')
+print("Seu nome é "+nome," sua idade é de "+idade, " seu peso é de "+peso)
+
+verific=input("Correto? ")
+if verific=='sim':
+        print('Bem Vinda: '+nome)
+else:
+        print('Informe seus dados novamente :')
